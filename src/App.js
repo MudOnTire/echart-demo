a64fa5b71771193bc51d449f1d6a03513c5eaabf
bruce_zhang <bruce_zhang@trendmicro.com>
2021-03-04 18:44:52 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,52 +2,12 @@ import React, { Component } from 'react'
 import { Drawer } from 'antd';
 import 'antd/dist/antd.css';
 import Chart from './chart';
-import graph from './data.json';
 
 export default class App extends Component {
 
   state = {
     drawerVisible: false,
     clickedItem: null
-  }
-
-  getOption = () => {
-    const option = {
-      tooltip: {},
-      legend: [{
-        data: graph.categories.map(function (a) {
-          return a.name;
-        })
-      }],
-      series: [
-        {
-          name: 'Les Miserables',
-          type: 'graph',
-          layout: 'none',
-          data: graph.nodes,
-          links: graph.links,
-          categories: graph.categories,
-          roam: true,
-          label: {
-            show: true,
-            position: 'right',
-            formatter: '{b}'
-          },
-          labelLayout: {
-            hideOverlap: true
-          },
-          scaleLimit: {
-            min: 0.4,
-            max: 2
-          },
-          lineStyle: {
-            color: 'source',
-            curveness: 0.3
-          }
-        }
-      ]
-    };
-    return option;
   }
 
   closeDrawer = () => {
@@ -77,10 +37,7 @@ export default class App extends Component {
 
     return (
       <>
-        <Chart
-          getOption={this.getOption}
-          onClick={this.handleClick}
-        />
+        <Chart onClick={this.handleClick} />
         <Drawer
           title="Basic Drawer"
           placement="right"
--- src/chart/index.js
+++ src/chart/index.js
@@ -1,15 +1,99 @@
 import React, { Component } from 'react'
 import ReactECharts from 'echarts-for-react';  // or var ReactECharts = require('echarts-for-react');
+import graph from '../data.json';
 
 export default class Chart extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    option: {
+      tooltip: {},
+      legend: [{
+        data: []
+      }],
+      series: [
+        {
+          name: 'Les Miserables',
+          type: 'graph',
+          layout: 'none',
+          data: [],
+          links: [],
+          categories: [],
+          roam: true,
+          label: {
+            show: true,
+            position: 'right',
+            formatter: '{b}'
+          },
+          labelLayout: {
+            hideOverlap: true
+          },
+          scaleLimit: {
+            min: 0.4,
+            max: 2
+          },
+          lineStyle: {
+            color: 'source',
+            curveness: 0.3
+          }
+        }
+      ]
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      console.log('chart ref', this.chart);
+      this.chart.getEchartsInstance().setOption({
+        legend: [{
+          data: graph.categories.map(function (a) {
+            return a.name;
+          })
+        }],
+        series: [
+          {
+            name: 'Les Miserables',
+            type: 'graph',
+            layout: 'none',
+            data: graph.nodes,
+            links: graph.links,
+            categories: graph.categories,
+            roam: true,
+            label: {
+              show: true,
+              position: 'right',
+              formatter: '{b}'
+            },
+            labelLayout: {
+              hideOverlap: true
+            },
+            scaleLimit: {
+              min: 0.4,
+              max: 2
+            },
+            lineStyle: {
+              color: 'source',
+              curveness: 0.3
+            }
+          }
+        ]
+      });
+    }, 1000);
+  }
+
   render() {
     const { getOption, onClick } = this.props;
+    const { option } = this.state;
     return (
       <ReactECharts
-        option={getOption()}
+        option={option}
         notMerge={true}
         lazyUpdate={true}
         theme={"theme_name"}
+        ref={(e) => { this.chart = e; }}
         style={{
           width: '100vw',
           height: '100vh',
