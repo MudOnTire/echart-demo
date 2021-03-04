f1e2c2806993a77838378abad1e630ee0147a96f
bruce_zhang <bruce_zhang@trendmicro.com>
2021-03-04 18:51:04 +0800

    finish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
 import React, { Component } from 'react'
 import { Drawer } from 'antd';
 import 'antd/dist/antd.css';
-import Chart from './chart';
+import ReactECharts from 'echarts-for-react';  // or var ReactECharts = require('echarts-for-react');
+import graph from './data.json';
 
 export default class App extends Component {
 
   state = {
     drawerVisible: false,
-    clickedItem: null
+    clickedItem: null,
+    option: {
+      tooltip: {},
+      legend: [{
+        data: []
+      }],
+      series: [
+        {
+          name: 'Les Miserables',
+          type: 'graph',
+          layout: 'none',
+          data: [],
+          links: [],
+          categories: [],
+          roam: true,
+          label: {
+            show: true,
+            position: 'right',
+            formatter: '{b}'
+          },
+          labelLayout: {
+            hideOverlap: true
+          },
+          scaleLimit: {
+            min: 0.4,
+            max: 2
+          },
+          lineStyle: {
+            color: 'source',
+            curveness: 0.3
+          }
+        }
+      ]
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.chart.getEchartsInstance().setOption({
+        legend: [{
+          data: graph.categories.map(function (a) {
+            return a.name;
+          })
+        }],
+        series: [
+          {
+            data: graph.nodes,
+            links: graph.links,
+            categories: graph.categories,
+          }
+        ]
+      });
+    }, 500);
   }
 
   closeDrawer = () => {
@@ -33,11 +86,24 @@ export default class App extends Component {
 
   render() {
 
-    const { drawerVisible, clickedItem } = this.state;
+    const { drawerVisible, clickedItem, option } = this.state;
 
     return (
       <>
-        <Chart onClick={this.handleClick} />
+        <ReactECharts
+          option={option}
+          notMerge={true}
+          lazyUpdate={true}
+          theme={"theme_name"}
+          ref={(e) => { this.chart = e; }}
+          style={{
+            width: '100vw',
+            height: '100vh',
+          }}
+          onEvents={{
+            click: this.handleClick
+          }}
+        />
         <Drawer
           title="Basic Drawer"
           placement="right"
